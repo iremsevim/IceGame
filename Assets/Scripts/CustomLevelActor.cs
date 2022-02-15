956d41cd1550667e679547c8f86c01946f121e63
iremsevim <iremsevim2442@outlook.com>
2022-02-15 14:31:25 +0300

    Animations Added.

--- Assets/Scripts/CustomLevelActor.cs
+++ Assets/Scripts/CustomLevelActor.cs
@@ -5,8 +5,10 @@ using Coskunerov.Actors;
 
 public class CustomLevelActor : LevelActor
 {
+    public Transform characterPoint;
     public override void SetupLevel()
     {
-       
+     GameObject character=Instantiate(GameData.Instance.gameDataSO.characterPrefab, characterPoint.position, Quaternion.identity);
+        character.transform.SetParent(transform);
     }
 }
---
+++ Assets/Scripts/GameGeneral/GameDataSO.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName ="Game Data",menuName ="Ice Game/Game Data")]
+public class GameDataSO : ScriptableObject
+{
+    public GameObject characterPrefab;
+}
