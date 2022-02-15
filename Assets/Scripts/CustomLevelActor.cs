811b73ce4b4017c76d70f3edc047cea3236f48cd
iremsevim <iremsevim2442@outlook.com>
2022-02-15 14:16:09 +0300

    Environment Added.

---
+++ Assets/Scripts/CustomLevelActor.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Coskunerov.Actors;
+
+public class CustomLevelActor : LevelActor
+{
+    public override void SetupLevel()
+    {
+       
+    }
+}
