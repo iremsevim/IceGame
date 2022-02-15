e3c740ccdd6ef614c8480ff9dc3f102f080c7816
iremsevim <iremsevim2442@outlook.com>
2022-02-15 17:55:33 +0300

    KeyBoard Added.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -12,16 +12,19 @@ public class PlayerController : GameSingleActor<PlayerController>
     [Header("Refs")]
     public float movementSpeed;
     public float rotateSpeed;
+    public bool isMovement = true;
     public override void ActorStart()
     {
         anim.SetBool("run", true);
     }
     public override void ActorFixedUpdate()
     {
+        if (!isMovement) return;
         Movement();
     }
     public void Movement()
     {
+      
         rb.velocity = new Vector3(inputManager.Result * rotateSpeed, rb.velocity.y, movementSpeed);
         float rot_y = Mathf.Atan2(rb.velocity.x, rb.velocity.z) * Mathf.Rad2Deg;
         rot_y = Mathf.Clamp(rot_y, -25, 25);
@@ -29,9 +32,26 @@ public class PlayerController : GameSingleActor<PlayerController>
 
 
     }
+    private void StopOrContinue(bool movementStatus)
+    {
+        if(movementStatus)
+        {
+            isMovement = false;
+            rb.velocity = Vector3.zero;
+        }
+    }
     public void OnTouchedIceGroupCollider(IceGroupCarrier touchedGroup)
     {
-
+        UIActor.Instance.ShowHideLetterPanel(true);
+        StopOrContinue(true);
+        anim.SetBool("run", false);
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.TryGetComponent(out ITriggerListener triggerListener))
+        {
+            triggerListener.OnTouched(this);
+        }
     }
 
 }
---
+++ Assets/Scripts/UI/UIActor.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+using Coskunerov.Actors;
+
+public class UIActor : GameSingleActor<UIActor>
+{
+    public RectTransform letterpanel;
+    private Vector3 letterPanelFirstPos;
+    public override void ActorAwake()
+    {
+        letterPanelFirstPos = letterpanel.GetComponent<RectTransform>().position;
+    }
+
+    public void ShowHideLetterPanel(bool status)
+    {
+        if(status)
+        {
+            letterpanel.DOLocalMoveY(letterPanelFirstPos.y, 1f);
+        }
+        else
+        {
+            letterpanel.DOMove(letterPanelFirstPos, 1f);
+        }
+    }
+}
